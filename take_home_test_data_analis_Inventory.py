1a67638bdec6a2d68ed6df27bb3d82c39a433205
Onky Prisma Pradana <freddycull27@gmail.com>
2025-09-22 10:17:04 +0700

    take_home_test_data_analis_Inventory.py

--- take_home_test_data_analis_Inventory.py
+++ take_home_test_data_analis_Inventory.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-url = "https://drive.google.com/https://drive.google.com/file/d/1DBbnyF4AvHJQqj6JdfM3IOoH9lCwaoRj/view?usp=drive_link"
+url = "https://drive.google.com/file/d/1DBbnyF4AvHJQqj6JdfM3IOoH9lCwaoRj/view?usp=drive_link"
 df = pd.read_csv(url)
 
