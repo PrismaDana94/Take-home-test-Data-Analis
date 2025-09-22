4213b6f0e546e748fcfd6704a4f545b7620535a1
Onky Prisma Pradana <freddycull27@gmail.com>
2025-09-22 10:47:24 +0700

    take_home_test_data_analis_Inventory.py

--- take_home_test_data_analis_Inventory.py
+++ take_home_test_data_analis_Inventory.py
@@ -1,17 +1,8 @@
-import gdown
 import pandas as pd
 
-# ID file Google Drive
-file_id = "1DBbnyF4AvHJQqj6JdfM3IOoH9lCwaoRj"
-url = f"https://drive.google.com/uc?id={file_id}"
-output = "inventory_clean.csv"
-
-# download file
-gdown.download(url, output, quiet=False)
-
-# baca file CSV
-df = pd.read_csv(output)
+df = pd.read_parquet("inventory_clean.parquet")
 print(df.head())
+
 
 
 
